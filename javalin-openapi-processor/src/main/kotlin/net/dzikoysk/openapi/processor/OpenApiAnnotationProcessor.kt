72dc8fd97b49dcd22e087d22fb617473a9aa600c
dzikoysk <dzikoysk@dzikoysk.net>
2021-06-22 01:51:56 +0200

    Generate openapi.json file by annotation-processor

--- javalin-openapi-processor/src/main/kotlin/net/dzikoysk/openapi/processor/OpenApiAnnotationProcessor.kt
+++ javalin-openapi-processor/src/main/kotlin/net/dzikoysk/openapi/processor/OpenApiAnnotationProcessor.kt
@@ -3,6 +3,7 @@ package net.dzikoysk.openapi.processor
 import net.dzikoysk.openapi.processor.annotations.OpenApiLoader
 import net.dzikoysk.openapi.processor.utils.ProcessorUtils
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Filer
 import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
@@ -11,6 +12,7 @@ import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
 import javax.tools.Diagnostic.Kind.WARNING
+import javax.tools.StandardLocation
 
 open class OpenApiAnnotationProcessor : AbstractProcessor() {
 
@@ -18,24 +20,32 @@ open class OpenApiAnnotationProcessor : AbstractProcessor() {
         lateinit var messager: Messager
         lateinit var elements: Elements
         lateinit var types: Types
+        lateinit var filer: Filer
     }
 
     override fun init(processingEnv: ProcessingEnvironment) {
         messager = processingEnv.messager
         elements = processingEnv.elementUtils
         types = processingEnv.typeUtils
+        filer = processingEnv.filer
         messager.printMessage(WARNING, "OpenApi Annotation Processor")
     }
 
     override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        if (roundEnv.processingOver()) {
+            return false
+        }
+
         try {
             val openApiAnnotations = OpenApiLoader.loadAnnotations(annotations, elements, types, roundEnv)
 
             val generator = OpenApiGenerator(messager)
             val result = generator.generate(openApiAnnotations)
-            messager.printMessage(WARNING, result)
 
-            // TODO: result to file
+            filer.createResource(StandardLocation.CLASS_OUTPUT, "", "openapi.json").openWriter().use {
+                it.write(result)
+                messager.printMessage(WARNING, result)
+            }
         } catch (throwable: Throwable) {
             ProcessorUtils.printException(throwable)
         }
