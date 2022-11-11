49d74e71bcb97e84db515cce85b1d61a32bdc029
dzikoysk <dzikoysk@dzikoysk.net>
2022-11-11 01:54:20 +0100

    GH-91 Attach Javalin implementation to annotation processor module

--- openapi-annotation-processor/build.gradle.kts
+++ openapi-annotation-processor/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     api(project(":openapi-specification"))
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    implementation("io.javalin:javalin:5.1.3")
     implementation("com.google.code.gson:gson:2.10")
     implementation("io.swagger.parser.v3:swagger-parser:2.1.7")
 
