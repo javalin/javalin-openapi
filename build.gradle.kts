eac3e915dad79cdec42d99c95a8b605007f45627
dzikoysk <dzikoysk@dzikoysk.net>
2022-11-16 23:03:59 +0100

    GH-101 Mark sources as 5.1.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "5.1.4-SNAPSHOT"
+    version = "5.1.5-SNAPSHOT"
 
     repositories {
         mavenCentral()
