275f99369e5f60e315fa6df872eb449d17bb0791
dzikoysk <dzikoysk@dzikoysk.net>
2022-12-02 14:45:09 +0100

    GH-115 Prototype of new configuration

--- javalin-plugins/javalin-openapi-plugin/src/main/kotlin/io/javalin/openapi/plugin/OpenApiPlugin.kt
+++ javalin-plugins/javalin-openapi-plugin/src/main/kotlin/io/javalin/openapi/plugin/OpenApiPlugin.kt
@@ -1,6 +1,7 @@
 package io.javalin.openapi.plugin
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import io.javalin.Javalin
 import io.javalin.json.JavalinJackson
@@ -12,19 +13,73 @@ import io.javalin.openapi.Security
 import io.javalin.openapi.SecurityScheme
 import io.javalin.plugin.Plugin
 import io.javalin.security.RouteRole
+import java.awt.SystemColor
+import java.util.function.BiConsumer
+import java.util.function.Consumer
 
+const val DEFAULT_DOCS_ID = "*"
+
+/** Configure OpenApi plugin */
 class OpenApiConfiguration {
-    var info: OpenApiInfo = OpenApiInfo()
-    var servers: Array<OpenApiServer> = emptyArray()
-    var documentationPath = "/openapi"
-    var documentProcessor: ((ObjectNode) -> String)? = null
-    var security: SecurityConfiguration? = null
-    var roles: Array<RouteRole> = emptyArray()
+
+    @JvmSynthetic internal var documentationPath = "/openapi"
+    @JvmSynthetic internal var roles: Array<out RouteRole> = emptyArray()
+    @JvmSynthetic internal var definitionConfiguration: BiConsumer<String, DefinitionConfiguration>? = null
+
+    /** Path to host documentation as JSON */
+    fun withDocumentationPath(path: String): OpenApiConfiguration = also {
+        this.documentationPath = path
+    }
+
+    /** List of roles eligible to access OpenApi routes */
+    fun withRoles(vararg roles: RouteRole): OpenApiConfiguration = also {
+        this.roles = roles
+    }
+
+    /* */
+    fun withDefinitionConfiguration(definitionConfigurationConfigurer: BiConsumer<String, DefinitionConfiguration>): OpenApiConfiguration = also {
+        definitionConfiguration = definitionConfigurationConfigurer
+    }
+
+}
+
+/** Modify OpenApi documentation represented by [ObjectNode] in JSON format */
+fun interface DefinitionProcessor {
+    fun process(content: ObjectNode): String
+}
+
+class DefinitionConfiguration {
+
+    internal var info: OpenApiInfo? = null
+    internal var servers: MutableList<OpenApiServer> = mutableListOf()
+    internal var security: SecurityConfiguration? = null
+    internal var definitionProcessor: DefinitionProcessor? = null
+
+    /** Define custom info object */
+    fun withOpenApiInfo(openApiInfo: Consumer<OpenApiInfo>): DefinitionConfiguration = also {
+        this.info = OpenApiInfo().also { openApiInfo.accept(it) }
+    }
+
+    /** Add custom server **/
+    fun withServer(serverConfigurer: Consumer<OpenApiServer>): DefinitionConfiguration = also {
+        this.servers.add(OpenApiServer().also { serverConfigurer.accept(it) })
+    }
+
+    /** Define custom security object */
+    fun withSecurity(securityConfiguration: SecurityConfiguration): DefinitionConfiguration = also {
+        this.security = securityConfiguration
+    }
+
+    /** Register scheme processor */
+    fun withDefinitionProcessor(definitionProcessor: DefinitionProcessor): DefinitionConfiguration = also {
+        this.definitionProcessor = definitionProcessor
+    }
+
 }
 
 data class SecurityConfiguration @JvmOverloads constructor(
-    val securitySchemes: MutableMap<String, SecurityScheme> = mutableMapOf(),
-    val globalSecurity: MutableList<Security> = mutableListOf()
+    internal val securitySchemes: MutableMap<String, SecurityScheme> = mutableMapOf(),
+    internal val globalSecurity: MutableList<Security> = mutableListOf()
 ) {
 
     fun withSecurityScheme(schemeName: String, securityScheme: SecurityScheme): SecurityConfiguration = also {
@@ -49,40 +104,45 @@ open class OpenApiPlugin @JvmOverloads constructor(private val configuration: Op
 
     private fun createDocumentation(app: Javalin): Lazy<Map<String, String>> =
         lazy {
-            with(configuration) {
-                val jsonMapper = when (val jsonMapper = app.jsonMapper()) {
-                    is JavalinJackson -> jsonMapper.mapper
-                    else -> JavalinJackson.defaultMapper()
-                }
-
-                OpenApiLoader()
-                    .loadOpenApiSchemes()
-                    .mapValues { (_, rawDocs) ->
-                        val docsNode = jsonMapper.readTree(rawDocs) as ObjectNode
-
-                        //process OpenAPI "info"
-                        docsNode.replace("info", jsonMapper.convertValue(info, JsonNode::class.java))
-
-                        // process OpenAPI "servers"
-                        docsNode.replace("servers", jsonMapper.convertValue(servers, JsonNode::class.java))
-
-                        // process OpenAPI "components"
-                        val componentsNode = docsNode.get("components") as? ObjectNode?
-                            ?: jsonMapper.createObjectNode().also { docsNode.replace("components", it) }
-
-                        // process OpenAPI "securitySchemes"
-                        val securitySchemes = security?.securitySchemes ?: emptyMap()
-                        componentsNode.replace("securitySchemes", jsonMapper.convertValue(securitySchemes, JsonNode::class.java))
-
-                        //process OpenAPI "security"
-                        val securityMap = security?.globalSecurity?.associate { it.name to it.scopes.toTypedArray() }
-                        docsNode.replace("security", jsonMapper.convertValue(securityMap, JsonNode::class.java))
-
-                        configuration.documentProcessor
-                            ?.invoke(docsNode)
-                            ?: docsNode.toPrettyString()
-                    }
+            val jsonMapper = when (val jsonMapper = app.jsonMapper()) {
+                is JavalinJackson -> jsonMapper.mapper
+                else -> JavalinJackson.defaultMapper()
             }
+
+            OpenApiLoader()
+                .loadOpenApiSchemes()
+                .mapValues { (version, rawDocs) ->
+                    configuration.definitionConfiguration
+                        ?.let { DefinitionConfiguration().also { definition -> it.accept(version, definition) } }
+                        ?.applyConfigurationTo(jsonMapper, version, rawDocs)
+                        ?: rawDocs
+                }
         }
+
+    private fun DefinitionConfiguration.applyConfigurationTo(jsonMapper: ObjectMapper, version: String, content: String): String {
+        val docsNode = jsonMapper.readTree(content) as ObjectNode
+
+        //process OpenAPI "info"
+        docsNode.replace("info", jsonMapper.convertValue(SystemColor.info, JsonNode::class.java))
+
+        // process OpenAPI "servers"
+        docsNode.replace("servers", jsonMapper.convertValue(servers, JsonNode::class.java))
+
+        // process OpenAPI "components"
+        val componentsNode = docsNode.get("components") as? ObjectNode?
+            ?: jsonMapper.createObjectNode().also { docsNode.replace("components", it) }
+
+        // process OpenAPI "securitySchemes"
+        val securitySchemes = security?.securitySchemes ?: emptyMap()
+        componentsNode.replace("securitySchemes", jsonMapper.convertValue(securitySchemes, JsonNode::class.java))
+
+        //process OpenAPI "security"
+        val securityMap = security?.globalSecurity?.associate { it.name to it.scopes.toTypedArray() }
+        docsNode.replace("security", jsonMapper.convertValue(securityMap, JsonNode::class.java))
+
+        return definitionProcessor
+            ?.process(docsNode)
+            ?: docsNode.toPrettyString()
+    }
 
 }
--- openapi-specification/src/main/kotlin/io/javalin/openapi/Server.kt
+++ openapi-specification/src/main/kotlin/io/javalin/openapi/Server.kt
@@ -6,14 +6,25 @@ import com.fasterxml.jackson.annotation.JsonProperty
 class OpenApiServer {
 
     /** REQUIRED. A URL to the target host. This URL supports Server Variables and MAY be relative, to indicate that the host location is relative to the location where the OpenAPI document is being served. Variable substitutions will be made when a variable is named in {brackets}. */
-    var url = ""
+    var url: String? = null
     /** An optional string describing the host designated by the URL. CommonMark syntax MAY be used for rich text representation. */
     var description: String? = null
     /** A map between a variable name and its value. The value is used for substitution in the server's URL template. */
     var variables: MutableMap<String, OpenApiServerVariable> = mutableMapOf()
 
-    fun addVariable(key: String, variable: OpenApiServerVariable) {
+    fun addVariable(key: String, variable: OpenApiServerVariable): OpenApiServer = also {
         variables[key] = variable
+    }
+
+    fun addVariable(key: String, defaultValue: String, values: Array<String>, description: String): OpenApiServer = also {
+        addVariable(
+            key = key,
+            variable = OpenApiServerVariable().also {
+                it.values = values
+                it.default = defaultValue
+                it.description = description
+            }
+        )
     }
 
 }
@@ -22,9 +33,9 @@ class OpenApiServer {
 class OpenApiServerVariable {
     /** An enumeration of string values to be used if the substitution options are from a limited set. The array MUST NOT be empty. */
     @JsonProperty("enum")
-    var values = arrayOf("")
+    var values: Array<String>? = null
     /** REQUIRED. The default value to use for substitution, which SHALL be sent if an alternate value is not supplied. Note this behavior is different than the Schema Object's treatment of default values, because in those cases parameter values are optional. If the enum is defined, the value MUST exist in the enum's values. */
-    var default = ""
+    var default: String? = null
     /** An optional description for the server variable. CommonMark syntax MAY be used for rich text representation. */
     var description: String? = null
 }
