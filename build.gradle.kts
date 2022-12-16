95a9a2e07d9b1de482d34b50ce513ce53b97ee8d
dzikoysk <dzikoysk@dzikoysk.net>
2022-12-16 17:45:04 +0100

    GH-117 Release 5.3.0-alpha.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "5.3.0-alpha.2"
+    version = "5.3.0-alpha.4-SNAPSHOT"
 
     repositories {
         mavenCentral()
