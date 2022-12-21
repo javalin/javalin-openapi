5a2d5e84882494b346bb032cff07cfc8156c6b10
dzikoysk <dzikoysk@dzikoysk.net>
2022-12-21 14:00:17 +0100

    GH-129 Cover JsonSchema composition usage with unit test

--- openapi-annotation-processor/src/test/kotlin/io/javalin/openapi/processor/specification/OpenApiAnnotationProcessorSpecification.kt
+++ openapi-annotation-processor/src/test/kotlin/io/javalin/openapi/processor/specification/OpenApiAnnotationProcessorSpecification.kt
@@ -1,18 +1,34 @@
 package io.javalin.openapi.processor.specification
 
+import io.javalin.openapi.JsonSchemaLoader
 import io.javalin.openapi.OpenApiLoader
 import org.junit.jupiter.api.Assertions
+
+typealias SchemeConsumer = (String) -> Unit
 
 internal abstract class OpenApiAnnotationProcessorSpecification {
 
     private companion object {
         val openApiSchemes = OpenApiLoader().loadOpenApiSchemes()
+        val jsonSchemes = JsonSchemaLoader().loadGeneratedSchemes()
     }
 
-    fun withOpenApi(name: String, consumer: (String) -> Unit) {
+
+    fun withOpenApi(name: String, consumer: SchemeConsumer) {
         openApiSchemes[name.replace(" ", "-")]
             ?.also { consumer(it) }
-            ?: run { Assertions.fail("Scheme '$name' not found") }
+            ?: failWithSchemeNotFound(name)
+    }
+
+    fun withJsonScheme(name: String, consumer: SchemeConsumer) {
+        jsonSchemes
+            .find { it.name.contains(name) }
+            ?.also { consumer(it.getContentAsString()) }
+            ?: failWithSchemeNotFound(name)
+    }
+
+    private fun failWithSchemeNotFound(name: String) {
+        Assertions.fail<Nothing>("Scheme '$name' not found")
     }
 
 }
