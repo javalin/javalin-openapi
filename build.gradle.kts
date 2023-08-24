8b3f86ac31ce70d01884ffc9080171409a128a30
dzikoysk <dzikoysk@dzikoysk.net>
2023-08-25 01:39:38 +0200

    Release 5.6.2-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "5.6.2"
+    version = "5.6.2-1"
 
     repositories {
         mavenCentral()
