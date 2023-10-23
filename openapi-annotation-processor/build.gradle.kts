d85a6a8b7e1bb2327c78ca7333cae6c7a592cc93
dzikoysk <dzikoysk@dzikoysk.net>
2023-10-23 17:40:44 +0200

    GH-202 Release 5.6.3 (Resolve #202)

--- openapi-annotation-processor/build.gradle.kts
+++ openapi-annotation-processor/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("org.apache.groovy:groovy:4.0.9")
 
-    implementation("io.javalin:javalin:5.6.2") {
+    implementation("io.javalin:javalin:5.6.3") {
         exclude(group = "org.slf4j")
     }
 
