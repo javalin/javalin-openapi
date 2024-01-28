5e2b0918b9ca7ad6ba3ee11c3ae24a5a18dd75e8
dzikoysk <dzikoysk@dzikoysk.net>
2024-01-28 15:41:33 +0100

    Mark sources as 6.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "6.0.0"
+    version = "6.0.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
