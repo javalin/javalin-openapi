2402338b3697437e00116f05d872429a3295ba43
dzikoysk <dzikoysk@dzikoysk.net>
2024-02-04 13:40:57 +0100

    Release 6.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "6.0.1-SNAPSHOT"
+    version = "6.0.1"
 
     repositories {
         mavenCentral()
@@ -110,7 +110,7 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     dependencies {
-        val javalin = "6.0.0"
+        val javalin = "6.0.1"
         compileOnly("io.javalin:javalin:$javalin")
         testImplementation("io.javalin:javalin:$javalin")
 
