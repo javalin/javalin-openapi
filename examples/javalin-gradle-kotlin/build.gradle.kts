20d05de2884123326989bfa09da5804bb8389c1b
dzikoysk <dzikoysk@dzikoysk.net>
2024-02-23 18:41:56 +0100

    Release 6.1.1

--- examples/javalin-gradle-kotlin/build.gradle.kts
+++ examples/javalin-gradle-kotlin/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     testImplementation("org.apache.groovy:groovy:4.0.12")
 
     // javalin
-    implementation("io.javalin:javalin:6.1.0")
+    implementation("io.javalin:javalin:6.1.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
     // logging
