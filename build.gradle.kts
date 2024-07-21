8577e4f6f24547c6473c5959fe2aa5518b1011be
dzikoysk <dzikoysk@dzikoysk.net>
2024-07-21 16:11:32 +0200

    GH-224 Mark sources as 6.2.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "6.2.0"
+    version = "6.2.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
