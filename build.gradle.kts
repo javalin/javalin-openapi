86faef0425381c1873ed085e3c6cb0f3a7ac154f
dzikoysk <dzikoysk@dzikoysk.net>
2025-11-29 19:00:50 +0100

    GH-250 Release 6.7.0-5

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "6.7.0-4"
+    version = "6.7.0-5"
 
     repositories {
         mavenCentral()
