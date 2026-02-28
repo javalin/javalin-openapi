4a073c88c365b6f4eaca6a8eeda0b0981bf8cb88
dzikoysk <dzikoysk@dzikoysk.net>
2026-02-28 21:07:37 +0100

    GH-274 Release 7.0.1 (Resolves #274)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "7.0.0"
+    version = "7.0.1"
 
     repositories {
         mavenCentral()
