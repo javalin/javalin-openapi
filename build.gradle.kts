b2f760ce0a49b5d68cde0e6a31126a98d8e7f89b
dzikoysk <dzikoysk@dzikoysk.net>
2026-05-14 21:42:24 +0200

    GH-283 Release 7.2.2 (Resolves #283)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "io.javalin.community.openapi"
-    version = "7.2.0"
+    version = "7.2.2"
 
     repositories {
         mavenCentral()
